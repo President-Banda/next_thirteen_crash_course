d4ec2031f99d3217f82c3c4e8723b1a55eb75216
president <abanda435@gmail.com>
2023-04-20 18:31:01 +0200

    added about page

---
+++ app/about/page.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>Welcome To Presidents Page</h1>
+      <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+      Eget a lacus egestas ultricies velit justo parturient feugiat 
+      facilisi cubilia sodales aliquet. Nulla lectus dapibus rhoncus 
+      nisi et pellentesque bibendum venenatis molestie in eu consectetur.
+      </p>
+    </div>
+  )
+}
+
+export default AboutPage
