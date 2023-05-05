61edabd23414a678183811ba265854a8a101d074
president <abanda435@gmail.com>
2023-05-05 22:20:27 +0200

    added repo component to main page

--- app/components/Repo.jsx
+++ app/components/Repo.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 async function fetchRepo(name) {
-  const response =  await fetch(`https://api.github.com/repos/${name}`);
+  const response =  await fetch(`https://api.github.com/repos/president-banda/${name}`);
 
   const repo = await response.json();
+
+  return repo;
 }
 
 const Repo = async ({ name }) => {
     const repo = await fetchRepo(name);
+    console.log(repo);
   return (
     <div>Repo</div>
   )
