cc660d320c0b6ca5d4c8497f6e7e8bbec6af79f1
president <abanda435@gmail.com>
2023-05-11 23:06:17 +0200

    added navigation to go back

--- app/code/repos/[name]/page.jsx
+++ app/code/repos/[name]/page.jsx
@@ -1,9 +1,13 @@
 import Repo from "@/app/components/Repo"
+import Link from "next/link"
 
 const RepoPage = ({ params :{ name } }) => {
     // console.log(params)
   return (
     <div className='card'>
+        <Link href='/code/repos' >
+          Back to Repositories
+        </Link>
         <Repo name={name} />
     </div>
   )
