17beb2bbb07d6531739b7f481b6ec5acb8569bf9
president <abanda435@gmail.com>
2023-05-14 17:33:31 +0200

    added search parameters to serach

--- app/api/courses/search/route.js
+++ app/api/courses/search/route.js
@@ -1,2 +1,8 @@
 import { NextResponse } from "next/server";
 import Courses from "../data.json";
+
+export async function GET(request){
+    const { searchParams } = new URL(request.url );
+    console.log(request.url);
+    return NextResponse.json(Courses)
+}
