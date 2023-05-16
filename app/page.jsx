efc57728556a3e3ae9269f497d7b39ce66a78247
president <abanda435@gmail.com>
2023-05-16 12:27:31 +0200

    added logic to search using a client component and manual lodaing using useEffect

--- app/page.jsx
+++ app/page.jsx
@@ -1,7 +1,29 @@
+'use client';
+import { useState, useEffect } from "react";
+import LoadingPage from "./loading";
 import Link from "next/link"
 import Courses from "./components/Courses"
 
 const HomePage = () => {
+
+  const [ courses, setCourses ] = useState([]);
+  const [ loading, setLoading ] = useState(true);
+
+  useEffect(() => {
+    const fetchCourses = async() => {
+      const response = await fetch('/api/courses');
+      const data = await response.json();
+      setCourses(data);
+      setLoading(false);
+    }
+
+    fetchCourses();
+  }, []);
+
+  if(loading){
+    return <LoadingPage />
+  }
+
   return (
     <div>
       <h1>Welcome To Presidents Crash Course</h1>
