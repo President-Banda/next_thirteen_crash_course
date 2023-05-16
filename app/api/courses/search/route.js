010960340f5b47c66db2ccb683fb77c104b2aa57
president <abanda435@gmail.com>
2023-05-16 09:29:20 +0200

    fixed courses not defined error

--- app/api/courses/search/route.js
+++ app/api/courses/search/route.js
@@ -4,5 +4,9 @@ import Courses from "../data.json";
 export async function GET(request){
     const { searchParams } = new URL(request.url );
     const query = searchParams.get("query");
-    return NextResponse.json(Courses)
+
+    const filteredCourses = Courses.filter((course) => {
+        return course.title.toLowerCase().includes(query.toLocaleLowerCase());
+    })
+    return NextResponse.json(filteredCourses);
 }
