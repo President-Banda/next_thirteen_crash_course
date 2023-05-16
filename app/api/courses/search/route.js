54941b5fe86d09a0e06538d465936f405b82317a
president <abanda435@gmail.com>
2023-05-16 09:26:24 +0200

    added course filter method for search

--- app/api/courses/search/route.js
+++ app/api/courses/search/route.js
@@ -3,6 +3,6 @@ import Courses from "../data.json";
 
 export async function GET(request){
     const { searchParams } = new URL(request.url );
-    console.log(request.url);
+    const query = searchParams.get("query");
     return NextResponse.json(Courses)
 }
