46807cbb10dc86fdd3e096be9b8b4ef962d35359
president <abanda435@gmail.com>
2023-05-16 12:07:33 +0200

    added uuid and post request to handle body data

--- app/api/courses/route.js
+++ app/api/courses/route.js
@@ -1,6 +1,23 @@
 import { NextResponse } from 'next/server'
 import courses from './data.json'
+import { v4 as uuid } from 'uuid';
 
 export async function GET(request){
     return NextResponse.json(courses)
+}
+
+export async function POST(request){
+    const { title, description, level, link } = await request.json();
+
+    const newCourse = {
+        id: uuidv4(),
+        title,
+        description,
+        level,
+        link,
+
+    };
+    console.log( title, description, level, link )
+    //courses.push(course);
+    return NextResponse.json({ 'message': "course created" });
 }
